19c02a263ad703ecbd61d4e0f9c1f282baf8f78c
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2020-04-06 18:01:58 +0200

    Create PasswordManager.py

---
+++ src/PasswordManager.py
@@ -0,0 +1,14 @@
+import hashlib
+import base64
+
+
+class pwdmanager():
+  
+   IsValid = False
+  
+  def __init__(self, master_password):
+    print('[*] Password Manager Has Been Loaded!')
+    if master_password == '1':
+      self.IsValid = True
+    else:
+      self.IsValid = False
